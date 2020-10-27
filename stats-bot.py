6738de144ab32fd8ca93535a48b99e59fc6e11ef
ThomasS0 <52140737+ThomasS0@users.noreply.github.com>
2020-10-27 17:58:15 -0300

    Create stats-bot.py

---
+++ stats-bot.py
@@ -0,0 +1,35 @@
+#notes: it doesn't send a message if you start the minecraft client, ONLY when you launch your server it'll send a message
+#also, the server must run in the same machine as the bot since this is a personal SMP stats bot
+import os
+import asyncio
+import discord
+
+#function to get the task names and try to find the server one("java.exe")
+def getTasks(name):
+    r = os.popen('tasklist /v').read().strip().split('\n')
+    print ('# of tasks is %s' % (len(r)))
+    for i in range(len(r)):
+        if name in r[i]:
+            return True
+    return False
+
+TOKEN = 'your-token-here'
+idserver = your-server-id
+idchat = your-chat-id
+
+client = discord.Client()
+process = 'java.exe'
+
+@client.event
+async def on_ready():
+    nm = 0
+    print(f'{client.user} has connected to Discord!')#confirming in the console that your bot is connected
+    print(f'{client.get_guild(idserver)} || server ||')#another confirmation but this one is to confirm the server id matches the desired server
+    while True:
+        if getTasks(process) and nm != -1:
+            await client.get_channel(idchat).send('server online!')
+            nm = -1
+        elif nm == -1 and getTasks(process) == False:
+            nm = 0
+
+client.run(TOKEN)
